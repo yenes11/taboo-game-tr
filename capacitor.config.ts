efab42c0764c6271b478fb0a6f23890c58136ba1
Enes Yurtsever <slothful_@outlook.com>
2022-04-22 15:05:50 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tabu',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
